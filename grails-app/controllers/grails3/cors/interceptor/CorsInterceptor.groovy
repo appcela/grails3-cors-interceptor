89ba430f12df556b530a26a371bcce003615f579
Ivan Chang <ichang@appcela.com>
2015-09-09 01:05:20 +0800

    added 'includedEnvironments', 'excludedEnvironments', and 'allowedOrigins' configuration settings support

--- grails-app/controllers/grails3/cors/interceptor/CorsInterceptor.groovy
+++ grails-app/controllers/grails3/cors/interceptor/CorsInterceptor.groovy
@@ -1,6 +1,7 @@
 package grails3.cors.interceptor
 
 import grails.compiler.GrailsCompileStatic
+import grails.util.Environment
 
 /**
  * Add Cross-Origin Resource Sharing (CORS) headers for Grails applications. These headers make it possible for
@@ -17,6 +18,21 @@ class CorsInterceptor {
     }
 
     boolean before() {
+        Map corsInterceptorConfig = (Map) grailsApplication.config.corsInterceptor
+        String[] includeEnvironments = corsInterceptorConfig['includeEnvironments']?: null
+        String[] excludeEnvironments = corsInterceptorConfig['excludeEnvironments']?: null
+        String[] allowedOrigins = corsInterceptorConfig['allowedOrigins']?: null
+
+        if( excludeEnvironments && excludeEnvironments.contains(Environment.current.name) )  { // current env is excluded
+            // skip
+            return true
+        }
+        else if( includeEnvironments && !includeEnvironments.contains(Environment.current.name) )  {  // current env is not included
+            // skip
+            return true
+        }
+
+
         String origin = request.getHeader("Origin");
         boolean options = ("OPTIONS" == request.method)
         if (options) {
@@ -28,8 +44,16 @@ class CorsInterceptor {
             }
         }
 
-        header("Access-Control-Allow-Origin", origin ?: "*")
-        header("Access-Control-Allow-Credentials", "true")
+        if( allowedOrigins && allowedOrigins.contains(origin)) { // request origin is on the white list
+            // add CORS access control headers for the given origin
+            header("Access-Control-Allow-Origin", origin)
+            header("Access-Control-Allow-Credentials", "true")
+        }
+        else if( !allowedOrigins ) { // no origin white list
+            // add CORS access control headers for all origins
+            header("Access-Control-Allow-Origin", origin ?: "*")
+            header("Access-Control-Allow-Credentials", "true")
+        }
 
         true // proceed to controller
     }
