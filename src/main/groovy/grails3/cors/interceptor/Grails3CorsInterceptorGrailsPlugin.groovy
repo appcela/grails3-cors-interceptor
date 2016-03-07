56e94d3c3218fbe9f52180e05715f95aa097591d
Ivan Chang <ichang@appcela.com>
2016-03-07 10:37:03 +0800

    updated "gradle.properties" to use Grails 3.0.15

--- src/main/groovy/grails3/cors/interceptor/Grails3CorsInterceptorGrailsPlugin.groovy
+++ src/main/groovy/grails3/cors/interceptor/Grails3CorsInterceptorGrailsPlugin.groovy
@@ -12,7 +12,7 @@ class Grails3CorsInterceptorGrailsPlugin extends Plugin {
     ]
 
     // TODO Fill in these fields
-    def version = "0.1.3" // must be added explicitly
+    def version = "0.1.4" // must be added explicitly
     def title = "Grails3 CORS Interceptor" // Headline display name of the plugin
     def author = "Ivan Chang"
     def authorEmail = "ichang@appcela.com"
